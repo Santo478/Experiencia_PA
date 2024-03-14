f175de554bf048ebceaf8a19e00f16b9ac265021
Santo478 <125693680+Santo478@users.noreply.github.com>
2024-03-14 23:55:35 +0000

    mejora adivinar_numer

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,16 +1,25 @@
 from random import *
 
 def adivinar_numero():
-    num = randint(1,10)
     while True:
-        guess = int(input('Adivina un número entre 1 y 10\n'))
-        if guess < 1 or guess > 10:
-            print('Introduce un número válido')
-        elif guess == num:
-            print('Adivinaste!')
+        num = randint(1,10)
+        while True:
+            guess = input('Adivina un número entre 1 y 10\n')
+            if guess not in "0123456789":
+                print("Introduce un número válido")
+
+            elif int(guess) < 1 or int(guess) > 10:
+                print('Introduce un número válido entre 1 y 10')
+            elif int(guess) == num:
+                print('Adivinaste!')
+                break
+            elif int(guess) != num:
+                print('Adivinaste mal. Intenta de nuevo')
+        replay = input("Quieres jugar de nuevo?\nY/N\n")
+        if replay not in "YN":
+            print("Opción no válida")
+        if replay == "N":
             break
-        elif guess != num:
-            print('Intenta de nuevo')
     """
     Esta función representa el juego de adivinar un número.
     Debes generar un número al azar entre 1 y 10, y luego pedir al usuario que adivine el número.
