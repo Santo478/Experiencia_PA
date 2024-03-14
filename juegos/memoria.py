d7bd15ee543bcdc49c3871dfb30468e7ad32e122
martin85646 <69407092+martin85646@users.noreply.github.com>
2024-03-14 18:47:24 +0000

    memoria y par o impar terminados

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,47 @@
+import random
+import time
+
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+    nivel = input("Elige un nivel de dificultad del 1 al 3: ")
+    lista = ["1","2","3"]
+    while True:
+        if nivel not in lista:
+            nivel = input("Elige un nivel de dificultad del 1 al 3: ")
+        else:
+            break
+    if nivel == "1":
+        numero = random.randint(1,10)
+        numero = str(numero)
+        print(f"Recuerda este numero: {numero}")
+        time.sleep(3)
+        for i in range(50):
+            print("")
+        guess = str(input("Cual es el numero que viste: "))
+        if guess == numero:
+            print("Ganaste!")
+        else:
+            print("Perdiste, intentalo de nuevo")
+    elif nivel == "2":
+        numero = random.randint(10000,10000000)
+        numero = str(numero)
+        print(f"Recuerda este numero: {numero}")
+        time.sleep(5)
+        for i in range(50):
+            print("")
+        guess = str(input("Cual es el numero que viste: "))
+        if guess == numero:
+            print("Felicidades, ganaste!")
+        else:
+            print("Perdiste, intentalo de nuevo")
+    else:
+        numero = random.randint(10000000000,100000000000000)
+        numero = str(numero)
+        print(f"Recuerda este numero: {numero}")
+        time.sleep(6)
+        for i in range(50):
+            print("")
+        guess = str(input("Cual es el numero que viste: "))
+        if guess == numero:
+            print("Felicidades, ganaste!")
+        else:
+            print("Perdiste, intentalo de nuevo")
